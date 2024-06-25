77b9c7b68e6666b44aa531faea3115cf916f6419
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-06-24 23:19:37 -0300

    Implementação do decorator para funções de inputs.

---
+++ decorador.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+def entrada(funcao):
+    def __envolucro(rotulo: str = '\n=> ', chamadas: int = 5, *args, **kwargs) -> Any:
+        """
+        Função de decoradora para 'inputs'.
+        :param rotulo: Descrição da entrada de dados.
+        :param chamadas: Número máximo de chamadas recursivas.
+        :return: Valor de entrada validado.
+        """
+        _entrada = funcao(input(rotulo), *args, **kwargs)
+        if _entrada is None:
+            if chamadas > 0:
+                return __envolucro(rotulo, chamadas - 1, *args, **kwargs)
+            else:
+                raise RuntimeError('<function: entrada> - Limite de chamadas recursivas atingido.')
+        return _entrada
+
+    return __envolucro
